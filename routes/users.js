71f62f3eac033a5e52ec7e4023eb354021a46c69
Filipe Frozza <filipefrozza.fm@gmail.com>
2019-03-26 15:06:54 -0300

    alterado o método Special para Check
    usado para verificar se o usuario está realmente logado

--- routes/users.js
+++ routes/users.js
@@ -10,8 +10,8 @@ routes.get('/', (req, res) => {
 routes.post('/register', userController.registerUser);
 routes.post('/login', userController.loginUser);
  
-routes.get('/special', passport.authenticate('jwt', { session: false }), (req, res) => {
-    return res.json({ msg: `Hey ${req.user.email}! I open at the close.` });
+routes.get('/check', passport.authenticate('jwt', { session: false }), (req, res) => {
+    return res.json({ msg: `${req.user.email} você está logado.` });
 });
  
 module.exports = routes;
